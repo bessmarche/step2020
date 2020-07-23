09cc4fc700c7e8cc30f285b273d32a776e516342
bessmarche <marchegiani.beatrice@gmail.com>
2020-07-23 15:23:30 +0000

    add infowindows for the markers

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -83,11 +83,48 @@ function deleteComment(id){
     fetch('/delete-data?idComment='+id, {method: 'POST'}).then(() => fetchData());
 }
 
+// function initialising map
 function createMap() {
+  var bistroInfo = '<div>'+
+      '<h1>Romeow Cat Bistro</h1>'+
+      'Cosy little vegan Bistro in a non touristy bit of the city! <br>'+
+      'Tasty food, welcoming staff and full of cute kittens! <br>'+
+      '<a href="http://www.romeowcatbistrot.com/">'+
+      'Romeow Cat Bistro website</a>'+
+      '</div>';
+
+  var gelatoInfo = '<div>'+
+      '<h1>Gelateria del Viale</h1>'+
+      'Traditional Gelato at a stone throw from the beautiful San Crisogono Basilica. <br>'+
+      'The owner, Mariagrazia, is one of the sweetest person you`ll ever meet! <br>'+
+      'I highly recomend the pistacchio and dark chocolate combination <br>'+
+      '<a href="https://www.tripadvisor.com/Restaurant_Review-g187791-d2353669-Reviews-Gelateria_del_Viale-Rome_Lazio.html">'+
+      'Check them on TripAdvisor</a>'+
+      '</div>';
+
+  var bakeryInfo ='<div>'+
+      '<h1>Pasticceria Boccione</h1>'+
+      'This family owned bakery is loceted at the hearth of the Jewish Getto in one of the most historical neighborood of the city. <br>'+
+      'Their produces offer a unique combination of the Jewish and Roman tradition! <br>'+
+      'My favourite cake if their richotta and cherry pie (100% reccomend)<br>'+
+      '<a href="https://www.tripadvisor.com/Attraction_Review-g187791-d2358692-Reviews-Pasticceria_Boccione-Rome_Lazio.html">'+
+      'Check them on TripAdvisor</a>'+
+      '</div>';
+
+  var bistroWindow = new google.maps.InfoWindow({
+    content: bistroInfo
+  });
+  var gelatoWindow = new google.maps.InfoWindow({
+    content: gelatoInfo
+  });
+  var bakeryWindow = new google.maps.InfoWindow({
+    content: bakeryInfo
+  });
+
   const map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 41.888300, lng: 12.481410}, zoom: 14});
-   const romeowMarker = new google.maps.Marker({
+   const bistroMarker = new google.maps.Marker({
       position: {lat: 41.870510, lng: 12.481530},
       map: map,
       title: 'Romeow Cat Bistro'
@@ -97,9 +134,19 @@ function createMap() {
       map: map,
       title: 'Best Gelato in Rome'
     }); 
-    const cakeMarker = new google.maps.Marker({
+    const bakeryMarker = new google.maps.Marker({
       position: {lat: 41.893230, lng: 12.476930},
       map: map,
       title: 'My favourite bakery'
     }); 
+
+    bistroMarker.addListener('click', function() {
+        bistroWindow.open(map, bistroMarker);
+    });
+    gelatoMarker.addListener('click', function() {
+        gelatoWindow.open(map, gelatoMarker);
+    });
+    bakeryMarker.addListener('click', function() {
+        bakeryWindow.open(map, bakeryMarker);
+    });
 }
