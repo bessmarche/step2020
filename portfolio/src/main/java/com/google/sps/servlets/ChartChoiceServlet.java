91fa3ab7d004ae6254e91918b80d79e951de15d8
bessmarche <marchegiani.beatrice@gmail.com>
2020-07-27 14:35:13 +0000

    rename variable

--- portfolio/src/main/java/com/google/sps/servlets/ChartChoiceServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartChoiceServlet.java
@@ -62,10 +62,10 @@ public class ChartChoiceServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String  vote = request.getParameter("vote");
-    Entity commentEntity = new Entity("Vote");
-    commentEntity.setProperty("voteValue", vote);
+    Entity voteEntity = new Entity("Vote");
+    voteEntity.setProperty("voteValue", vote);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.put(commentEntity);
+    datastore.put(voteEntity);
     //int currentVotes = chartVotes.containsKey(vote) ? chartVotes.get(vote) : 0;
     //chartVotes.put(vote, currentVotes + 1);
 
