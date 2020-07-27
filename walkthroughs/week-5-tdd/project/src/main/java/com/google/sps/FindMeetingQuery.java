32312c7d3e66023ee356fb02dc017c14eac4b181
bessmarche <marchegiani.beatrice@gmail.com>
2020-07-27 14:28:18 +0000

    make all tests in FindMeetingQueryTest pass

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,56 @@
 
 package com.google.sps;
 
+import java.io.*; 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+
 
 public final class FindMeetingQuery {
+  /*
+   ** query IN: collection of events  and details of meeting request. OUT:available times for the meeting
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+      Collection<String> attendees = request.getAttendees();
+      long meetingDuration = request.getDuration();
+      ArrayList<TimeRange> meetingTimes = new ArrayList<TimeRange>();
+      int start = TimeRange.START_OF_DAY;
+      int end = TimeRange.END_OF_DAY;
+    
+    if (attendees.isEmpty()){
+        meetingTimes.add(TimeRange.WHOLE_DAY);
+        return meetingTimes;
+    }
+
+    if (meetingDuration > TimeRange.END_OF_DAY){
+        return meetingTimes;
+    }
+
+    for(Event event : events){
+        //check the event has participants that are in the attendees
+        Collection<String> eventAttendees = event.getAttendees();
+        if(!(Collections.disjoint(attendees, eventAttendees))){
+          int eventStart = event.getWhen().start();
+          int eventEnd = event.getWhen().end();
+          //add time before thestart of this event
+          if (start + meetingDuration <= eventStart){
+             meetingTimes.add(TimeRange.fromStartEnd(start, eventStart, false));
+          }
+          if(start < eventEnd)
+                start =  eventEnd;
+        }
+    }
+
+    //add time between last event and  end of the day
+    if (start + meetingDuration <= TimeRange.END_OF_DAY){
+             meetingTimes.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+          }
+    return meetingTimes;
   }
 }
